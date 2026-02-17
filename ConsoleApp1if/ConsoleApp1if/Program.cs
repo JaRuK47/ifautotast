d830581a441273e55b3c16a2cf9200d4663e3069
KuRaJ <Kuraj.com>
2026-02-17 12:18:29 +0800

    Decomp

--- ConsoleApp1if/ConsoleApp1if/Program.cs
+++ ConsoleApp1if/ConsoleApp1if/Program.cs
@@ -1,53 +1,71 @@
-﻿int cop = int.Parse(Console.ReadLine());
-
-string text;
-string normrub;
-string normcop;
-
-if (cop % 100 > 4)
+﻿namespace FirstApp
 {
-    normcop = "копеек";
-}
-else if (cop == 1)
-{
-    normcop = "копейка";
-}
-else
-{
-    normcop = "копейки";
-}
-
-if(cop >= 100)
-{
-
-    int rub = cop / 100;
-    int ost = cop % 100;
-
-    if (rub%100 == 1)
+    public class Logic
     {
-        normrub = "рубль";
-    }
-    else if (rub % 100 < 5)
-    {
-        normrub = "рубля";
-    }
-    else
-    {
-        normrub = "рублей";
-    }
+        public static string Compare(int cop)
+        {
+            string text;
+            string normrub;
+            string normcop;
 
-    if (ost > 0)
-    {
-       text = rub + " " + normrub + "  " + ost + " " + normcop;
+            if (cop % 100 > 4)
+            {
+                normcop = "копеек";
+            }
+            else if (cop == 1)
+            {
+                normcop = "копейка";
+            }
+            else
+            {
+                normcop = "копейки";
+            }
+
+            if (cop >= 100)
+            {
+
+                int rub = cop / 100;
+                int ost = cop % 100;
+
+                if (rub % 100 == 1)
+                {
+                    normrub = "рубль";
+                }
+                else if (rub % 100 < 5)
+                {
+                    normrub = "рубля";
+                }
+                else
+                {
+                    normrub = "рублей";
+                }
+
+                if (ost > 0)
+                {
+                    text = rub + " " + normrub + "  " + ost + " " + normcop;
+                }
+                else
+                {
+                    text = rub + " " + normrub;
+                }
+            }
+            else
+            {
+                text = cop + " " + normcop;
+            }
+            return text;
+        }
     }
-    else 
+    class Program
     {
-       text = rub + " " + normrub;
+        static void Main(string[] args)
+        {
+
+            int cop = int.Parse(Console.ReadLine());
+
+            string text = Logic.Compare(cop);
+
+            Console.WriteLine(text);
+        }
     }
 }
-else
-{
-    text = cop + " " + normcop;
-}
-
-Console.WriteLine(text);
