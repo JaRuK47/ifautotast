f347e4c2a34f7e0b21fe77ed62f0dd539c479ded
KuRaJ <Kuraj.com>
2026-02-17 10:33:34 +0800

    first commit

--- ConsoleApp1if/ConsoleApp1if/Program.cs
+++ ConsoleApp1if/ConsoleApp1if/Program.cs
@@ -1,1 +1,53 @@
-﻿Console.WriteLine("Hello, World!");
+﻿int cop = int.Parse(Console.ReadLine());
+
+string text;
+string normrub;
+string normcop;
+
+if (cop % 100 > 4)
+{
+    normcop = "копеек";
+}
+else if (cop == 1)
+{
+    normcop = "копейка";
+}
+else
+{
+    normcop = "копейки";
+}
+
+if(cop >= 100)
+{
+
+    int rub = cop / 100;
+    int ost = cop % 100;
+
+    if (rub%100 == 1)
+    {
+        normrub = "рубль";
+    }
+    else if (rub % 100 < 5)
+    {
+        normrub = "рубля";
+    }
+    else
+    {
+        normrub = "рублей";
+    }
+
+    if (ost > 0)
+    {
+       text = rub + " " + normrub + "  " + ost + " " + normcop;
+    }
+    else 
+    {
+       text = rub + " " + normrub;
+    }
+}
+else
+{
+    text = cop + " " + normcop;
+}
+
+Console.WriteLine(text);
--- ConsoleApp1if/ConsoleApp1if/obj/Debug/net8.0/ConsoleApp1if.AssemblyInfo.cs
+++ ConsoleApp1if/ConsoleApp1if/obj/Debug/net8.0/ConsoleApp1if.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1if")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfd16bc764f90460c93d54966b0144c192576447")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1if")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1if")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
